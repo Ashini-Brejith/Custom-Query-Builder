15c5e6609e9a0fcf2eec4c549ad56d9b1f7eeacc
Ashini Brejith <ashinibrejith@MINAB279.local>
2025-01-13 16:54:06 +0530

    Updated create queries

--- query_builder_frontend/src/app/create-queries/create-queries.component.ts
+++ query_builder_frontend/src/app/create-queries/create-queries.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-create-queries',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './create-queries.component.html',
   styleUrl: './create-queries.component.css',
 })
 export class CreateQueriesComponent {
+	  tableName: string = '';
+	  columns: Array<any> = [
+    {
+      name: '',
+      type: 'int',
+      length: null,
+      notNull: false,
+      primaryKey: false,
+      unique: false,
+    },
+  ]
+ 
 
+  addColumn(): void {
+    this.columns.push({
+      name: '',
+      type: 'int',
+      length: null,
+      notNull: false,
+      primaryKey: false,
+      unique: false,
+    });
+  }
+
+  removeColumn(): void {
+    if (this.columns.length > 1) {
+      this.columns.pop();
+    }
+  }
+
+  onSubmit(): void {
+    console.log('Form Submitted', this.columns);
+    alert(`CREATE TABLE ${this.tableName} (${this.columns.map((c)=>`${c.name} ${c.type.toUpperCase()}`).join(',')})`);
+  }
 }
